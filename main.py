97f6936be2a26dfa44a4700e3275bee8b325158f
BoySona <154109769+Boysona@users.noreply.github.com>
2025-08-22 21:36:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ WELCOME_TEMPLATE = (
     " OK sand me Your media file audio video or voice massage Transcribe, translate & summarize effortlessly!\n"
     "Upload any media file (voice recordings, audio clips, or videos) up 20MB in size , in any language.\n\n"
     "Ensure the Output Language matches the language of your audio file for accurate transcription.\n\n"
-    "Other useful bot: @TextToSpeech_robot\n\n"
+    "Other useful bot: @TextToSpeechBBot\n\n"
     "🌐 Current Language: {lang_name}  My last update: 21/08/2025"
 )
 
@@ -628,7 +628,7 @@ def handle_text_messages(message):
     if message.chat.type == 'private' and str(message.from_user.id) != str(ADMIN_ID) and not check_subscription(message.from_user.id):
         send_subscription_message(message.chat.id)
         return
-    bot.send_message(message.chat.id, "I don’t support text-to-Speech. If you need text-to-voice, please use this bot: https://t.me/TextToSpeech_robot")
+    bot.send_message(message.chat.id, "I don’t support text-to-Speech. If you need text-to-voice, please use this bot: https://t.me/TextToSpeechBBot")
 
 @bot.message_handler(func=lambda m: True, content_types=['sticker', 'photo'])
 def handle_unsupported_media_types(message):
